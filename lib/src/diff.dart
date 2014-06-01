fddda439b67b5eafdc2551b1f225f7157b2cff53
Adam Singer <financeCoding@gmail.com>
2014-06-01 07:39:54 -0700

    format file

--- lib/src/diff.dart
+++ lib/src/diff.dart
@@ -1,44 +1,45 @@
 part of diff;
 
 class CandidateThing {
-    int file1index;
-    int file2index;
-    CandidateThing chain;
+  int file1index;
+  int file2index;
+  CandidateThing chain;
 }
 
 class commonOrDifferentThing {
-    List<String> common;
-    List<String> file1;
-    List<String> file2;
+  List<String> common;
+  List<String> file1;
+  List<String> file2;
 }
 
 class patchDescriptionThing {
-    patchDescriptionThing() { }
+  patchDescriptionThing() {}
 
-    patchDescriptionThing.fromFile(List<String> file, int offset, int length) {
-        Offset = offset;
-        Length = length;
-        Chunk = new List<String>.from(file.getRange(offset, offset + length).toList());
-    }
+  patchDescriptionThing.fromFile(List<String> file, int offset, int length) {
+    Offset = offset;
+    Length = length;
+    Chunk = new List<String>.from(file.getRange(offset, offset + length).toList(
+        ));
+  }
 
-    int Offset;
-    int Length;
-    List<String> Chunk;
+  int Offset;
+  int Length;
+  List<String> Chunk;
 }
 
 class patchResult {
-      patchDescriptionThing file1;
-      patchDescriptionThing file2;
+  patchDescriptionThing file1;
+  patchDescriptionThing file2;
 }
 
 class chunkReference {
-    int offset;
-    int length;
+  int offset;
+  int length;
 }
 
 class diffSet {
-    chunkReference file1;
-    chunkReference file2;
+  chunkReference file1;
+  chunkReference file2;
 }
 
 class Side<int> extends Enum<int> implements Comparable<Side<int>> {
@@ -56,34 +57,34 @@ class Side<int> extends Enum<int> implements Comparable<Side<int>> {
 }
 
 class diff3Set implements Comparable<diff3Set> {
-    Side side;
-    int file1offset;
-    int file1length;
-    int file2offset;
-    int file2length;
+  Side side;
+  int file1offset;
+  int file1length;
+  int file2offset;
+  int file2length;
 
-    int compareTo(diff3Set other) {
-        if (file1offset != other.file1offset)
-            return file1offset.compareTo(other.file1offset);
-        else
-            return side.compareTo(other.side);
+  int compareTo(diff3Set other) {
+    if (file1offset != other.file1offset) {
+      return file1offset.compareTo(other.file1offset);
+    } else {
+      return side.compareTo(other.side);
     }
+  }
 }
 
 class patch3Set {
-    Side side;
-    int offset;
-    int length;
-    int conflictOldOffset;
-    int conflictOldLength;
-    int conflictRightOffset;
-    int conflictRightLength;
+  Side side;
+  int offset;
+  int length;
+  int conflictOldOffset;
+  int conflictOldLength;
+  int conflictRightOffset;
+  int conflictRightLength;
 }
 
 class conflictRegion {
-    int file1RegionStart;
-    int file1RegionEnd;
-    int file2RegionStart;
-    int file2RegionEnd;
+  int file1RegionStart;
+  int file1RegionEnd;
+  int file2RegionStart;
+  int file2RegionEnd;
 }
-
