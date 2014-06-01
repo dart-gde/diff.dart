616eef37b7216969fdb36dc9728f652b75f9f80a
Adam Singer <financeCoding@gmail.com>
2014-06-01 08:02:56 -0700

    Adding method signatures

--- lib/src/diff.dart
+++ lib/src/diff.dart
@@ -113,3 +113,77 @@ class MergeConflictResultBlock implements IMergeResultBlock {
   List<String> RightLines;
   int RightIndex;
 }
+
+//
+// Methods
+//
+
+CandidateThing longest_common_subsequence(List<String> file1, List<String> file2) {
+  throw new UnimplementedError();
+}
+
+// TODO(adam): make this a closure and do not pass common;
+//void processCommon(ref commonOrDifferentThing common, List<commonOrDifferentThing> result) {
+//  throw new UnimplementedError();
+//}
+
+List<commonOrDifferentThing> diff_comm(List<String> file1, List<String> file2) {
+  throw new UnimplementedError();
+}
+
+List<patchResult> diff_patch(List<String> file1, List<String> file2) {
+  throw new UnimplementedError();
+}
+
+List<patchResult> strip_patch(List<patchResult> patch) {
+  throw new UnimplementedError();
+}
+
+void invert_patch(List<patchResult> patch) {
+  throw new UnimplementedError();
+}
+
+// TODO(adam): make this a closure
+//void copyCommon(int targetOffset, ref int commonOffset, string[] file, List<string> result)  {
+//
+//}
+
+List<String> patch(List<String> file, List<patchResult> patch) {
+  throw new UnimplementedError();
+}
+
+List<String> diff_merge_keepall(List<String> file1, List<String> file2) {
+  throw new UnimplementedError();
+}
+
+List<diffSet> diff_indices(List<String> file1, List<String> file2) {
+  throw new UnimplementedError();
+}
+
+// TODO(adam): make private
+void addHunk(diffSet h, Side side, List<diff3Set> hunks) {
+  throw new UnimplementedError();
+}
+
+// TODO(adam): make this a closure
+//void copyCommon2(int targetOffset, ref int commonOffset, List<patch3Set> result) {
+//
+//}
+
+List<patch3Set> diff3_merge_indices(List<String> a, List<String> o, List<String> b) {
+  throw new UnimplementedError();
+}
+
+// TODO(adam): make private
+void flushOk(List<String> okLines, List<IMergeResultBlock> result) {
+  throw new UnimplementedError();
+}
+
+// TODO(adam): make closure
+//bool isTrueConflict(patch3Set rec, string[] a, string[] b)  {
+//
+//}
+
+List<IMergeResultBlock> diff3_merge(List<String> a, List<String> o, List<String> b, bool excludeFalseConflicts)  {
+  throw new UnimplementedError();
+}
