dd6dfbd179088feaaf3ce59d505ca4fdccd40317
Adam Singer <financeCoding@gmail.com>
2014-06-01 17:31:22 -0700

    Adding unit test for diff3_dig

---
+++ test/src/diff3_dig_test.dart
@@ -0,0 +1,52 @@
+library diff3_dig_test;
+
+import "package:unittest/unittest.dart";
+
+import "package:diff/diff.dart";
+
+void defineTests() {
+  group('diff3_dig', () {
+    test('multiline difference', () {
+      // base
+      String o = "AA ZZ 00 M 99".split(" ").join("\n");
+
+      // ours
+      String a = "AA a b c ZZ new 00 a a M 99".split(" ").join("\n");
+
+      // theirs
+      String b = "AA a d c ZZ 11 M z z 99".split(" ").join("\n");
+
+      Diff3DigResult diff3DigResult = diff3_dig(a, o, b);
+
+      expect(diff3DigResult, isNotNull);
+      String expectResultText = """AA
+a
+<<<<<<<<<
+b
+=========
+d
+>>>>>>>>>
+c
+ZZ
+<<<<<<<<<
+new
+00
+a
+a
+=========
+11
+>>>>>>>>>
+M
+z
+z
+99""";
+      String actualResultText = diff3DigResult.Text.join("\n");
+      expect(actualResultText, equals(expectResultText));
+    });
+  });
+
+}
+
+void main() {
+  defineTests();
+}
--- test/test_runner.dart
+++ test/test_runner.dart
@@ -2,8 +2,10 @@ library test_runner;
 
 import "src/side_enum_test.dart" as side_enum_test;
 import "src/diff3_set_test.dart" as diff3_set_test;
+import "src/diff3_dig_test.dart" as diff3_dig_test;
 
 void main() {
   side_enum_test.main();
   diff3_set_test.main();
+  diff3_dig_test.main();
 }
